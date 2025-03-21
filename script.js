6120c4335ed280238d9417efdfd01be1d625c136
meps106130 <120775416+meps106130@users.noreply.github.com>
2025-03-22 02:21:56 +0800

    upload

--- script.js
+++ script.js
@@ -321,3 +321,9 @@ function loadPage(page) {
         });
     }
 }
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+document.addEventListener("keydown", (event) => {
+    if (event.key === "F12" || (event.ctrlKey && event.shiftKey && event.key === "I")) {
+        event.preventDefault();
+    }
+});
